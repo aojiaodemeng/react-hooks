907748ede1f5fcb3c0408a750c557a2f1f78e5ff
chenxinyi <chenxinyi@vikadata.com>
2021-07-28 15:03:42 +0800

    feat: 实现useEffect方法

--- src/App.js
+++ src/App.js
@@ -22,17 +22,55 @@ function useState(initialState) {
 
 function render() {
   stateIndex = 0;
+  effectIndex = 0;
   ReactDOM.render(<App />, document.getElementById("root"));
+}
+
+// 上一次的依赖值
+let prevDepsAry = [];
+let effectIndex = 0;
+function useEffect(callback, depsAry) {
+  // 判断callback是不是函数
+  if (Object.prototype.toString.call(callback) !== "[object Function]")
+    throw new Error("useEffect函数的第一个参数必须是函数");
+  // 判断depsAry有没有被传递
+  if (typeof depsAry === "undefined") {
+    // 没有传递
+    callback();
+  } else {
+    // 判断depsAry是不是数组
+    if (Object.prototype.toString.call(depsAry) !== "[object Array]")
+      throw new Error("useEffect函数的第二个参数必须是数组");
+    // 获取上一次的状态值
+    let prevDeps = prevDepsAry[effectIndex];
+    // 将当前的依赖值和上一次的依赖值做对比 如果有变化 调用callback
+    let hasChanged = prevDeps
+      ? depsAry.every((dep, index) => dep === prevDeps[index]) === false
+      : true;
+    // 判断值是否有变化
+    if (hasChanged) {
+      callback();
+    }
+    // 同步依赖值
+    prevDepsAry[effectIndex] = depsAry;
+    effectIndex++;
+  }
 }
 
 function App() {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("san");
+  useEffect(() => {
+    console.log("hhhhh");
+  }, [count]);
+  useEffect(() => {
+    console.log("name");
+  }, [name]);
   return (
     <>
       <div>
         {name}
-        <button onClick={() => setName("si")}>setCount</button>
+        <button onClick={() => setName("si")}>setName</button>
       </div>
       <div>
         {count}
