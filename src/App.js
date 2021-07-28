8be09611bd837c17213691e182c2666b2a366092
chenxinyi <chenxinyi@vikadata.com>
2021-07-28 15:19:49 +0800

    feat: 实现useReducer方法

--- src/App.js
+++ src/App.js
@@ -56,27 +56,32 @@ function useEffect(callback, depsAry) {
     effectIndex++;
   }
 }
-
+function useReducer(reducer, initialState) {
+  const [state, setState] = useState(initialState);
+  function dispatch(action) {
+    const newState = reducer(state, action);
+    setState(newState);
+  }
+  return [state, dispatch];
+}
 function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("san");
-  useEffect(() => {
-    console.log("hhhhh");
-  }, [count]);
-  useEffect(() => {
-    console.log("name");
-  }, [name]);
+  function reducer(state, action) {
+    switch (action.type) {
+      case "increment":
+        return state + 1;
+      case "decrement":
+        return state - 1;
+      default:
+        return state;
+    }
+  }
+  const [count, dispatch] = useReducer(reducer, 0);
   return (
-    <>
-      <div>
-        {name}
-        <button onClick={() => setName("si")}>setName</button>
-      </div>
-      <div>
-        {count}
-        <button onClick={() => setCount(count + 1)}>setCount</button>
-      </div>
-    </>
+    <div>
+      {count}
+      <button onClick={() => dispatch({ type: "increment" })}>+1</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-1</button>
+    </div>
   );
 }
 export default App;
--- static/README.md
+++ static/README.md
@@ -237,3 +237,40 @@ function App() {
 }
 export default App;
 ```
+
+# useReducer 实现原理
+
+```javascript
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+function useReducer(reducer, initialState) {
+  const [state, setState] = useState(initialState);
+  function dispatch(action) {
+    const newState = reducer(state, action);
+    setState(newState);
+  }
+  return [state, dispatch];
+}
+function App() {
+  function reducer(state, action) {
+    switch (action.type) {
+      case "increment":
+        return state + 1;
+      case "decrement":
+        return state - 1;
+      default:
+        return state;
+    }
+  }
+  const [count, dispatch] = useReducer(reducer, 0);
+  return (
+    <div>
+      {count}
+      <button onClick={() => dispatch({ type: "increment" })}>+1</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-1</button>
+    </div>
+  );
+}
+export default App;
+```
